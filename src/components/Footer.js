0ca0c3033b547259b7f8a375a614204b0a7868bd
lettiagc <letti.agc@gmail.com>
2023-12-02 20:55:25 +0100

    Adjusted Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,114 +4,54 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-        </p>
-        <div>
-          <form className="input-areas">
-            <input
-              type="email"
-              name="email"
-              placeholder="Your email"
-              className="footer-input"
-            />
-          </form>
-        </div>
-      </section>
-      <div className="footer-links">
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works Changed</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
-          </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
-          </div>
-        </div>
+    <footer className="footer">
+      <div className="footer__container">
+        <Link to="/">
+          <div className="footer__logo"></div>
+        </Link>
+        <div className="footer-divider"></div>
+        <ul className="footer__links">
+          <li className="footer__link-item">
+            <Link to="/tournaments" className="footer__link text-shadow">
+              Tournament Hub
+            </Link>
+          </li>
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link text-shadow">
+              Duelist Zone
+            </Link>
+          </li>
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link text-shadow">
+              Leaderboard
+            </Link>
+          </li>
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link text-shadow">
+              Rules
+            </Link>
+          </li>
+        </ul>
+        <div className="footer-divider"></div>
+        <ul className="footer__links">
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link-secondary text-shadow">
+              FAQ
+            </Link>
+          </li>
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link-secondary text-shadow">
+              Support
+            </Link>
+          </li>
+          <li className="footer__link-item">
+            <Link to="/" className="footer__link-secondary text-shadow">
+              Account
+            </Link>
+          </li>
+        </ul>
       </div>
-      <section class="social-media">
-        <div class="social-media-wrap">
-          <div class="footer-logo">
-            <Link to="/" className="social-logo">
-              TRVL
-              <i class="fab fa-typo3" />
-            </Link>
-          </div>
-          <small class="website-rights">TRVL © 2020</small>
-          <div class="social-icons">
-            <Link
-              class="social-icon-link facebook"
-              to="/"
-              target="_blank"
-              aria-label="Facebook"
-            >
-              <i class="fab fa-facebook-f" />
-            </Link>
-            <Link
-              class="social-icon-link instagram"
-              to="/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <i class="fab fa-instagram" />
-            </Link>
-            <Link
-              class="social-icon-link youtube"
-              to="/"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube" />
-            </Link>
-            <Link
-              class="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <i class="fab fa-twitter" />
-            </Link>
-            <Link
-              class="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <i class="fab fa-linkedin" />
-            </Link>
-          </div>
-        </div>
-      </section>
-    </div>
+    </footer>
   );
 }
 
